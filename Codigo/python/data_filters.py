24adcbc18ea88c7fd3728419198bb8bd3b3afd56
Diego Luis Di Salvatore Veiga <disalvatorediego@gmail.com>
2021-04-11 17:30:43 +0200

    [python] Funciones de orden superior (Ejercicios)

---
+++ Codigo/python/data_filters.py
@@ -0,0 +1,95 @@
+DATA = [
+    {
+        'name': 'Facundo',
+        'age': 72,
+        'organization': 'Platzi',
+        'position': 'Technical Coach',
+        'language': 'python',
+    },
+    {
+        'name': 'Luisana',
+        'age': 33,
+        'organization': 'Globant',
+        'position': 'UX Designer',
+        'language': 'javascript',
+    },
+    {
+        'name': 'Héctor',
+        'age': 19,
+        'organization': 'Platzi',
+        'position': 'Associate',
+        'language': 'ruby',
+    },
+    {
+        'name': 'Gabriel',
+        'age': 20,
+        'organization': 'Platzi',
+        'position': 'Associate',
+        'language': 'javascript',
+    },
+    {
+        'name': 'Isabella',
+        'age': 30,
+        'organization': 'Platzi',
+        'position': 'QA Manager',
+        'language': 'java',
+    },
+    {
+        'name': 'Karo',
+        'age': 23,
+        'organization': 'Everis',
+        'position': 'Backend Developer',
+        'language': 'python',
+    },
+    {
+        'name': 'Ariel',
+        'age': 32,
+        'organization': 'Rappi',
+        'position': 'Support',
+        'language': '',
+    },
+    {
+        'name': 'Juan',
+        'age': 17,
+        'organization': '',
+        'position': 'Student',
+        'language': 'go',
+    },
+    {
+        'name': 'Pablo',
+        'age': 32,
+        'organization': 'Master',
+        'position': 'Human Resources Manager',
+        'language': 'python',
+    },
+    {
+        'name': 'Lorena',
+        'age': 56,
+        'organization': 'Python Organization',
+        'position': 'Language Maker',
+        'language': 'python',
+    },
+]
+
+def run():
+    # Comprehensions
+    all_platzi_workers = [worker["name"] for worker in DATA if worker["organization"] == "Platzi"]
+    print("============")
+    print("TRABAJADORES DE PLATZI:")
+    print(all_platzi_workers)
+    
+    print("============")
+    print("LENGUAJES DE LOS TRABAJADORES:")
+    workers_languages = [worker["language"] for worker in DATA]
+    print(workers_languages) 
+
+    # High order functions
+    adults = list(filter(lambda worker: worker["age"] > 18, DATA))
+    adults_names = list(map(lambda worker: worker["name"], adults))
+    print("============")
+    print("NOMBRES DE ADULTOS:")
+    print(adults_names)
+
+    
+if __name__ == '__main__':
+    run()
